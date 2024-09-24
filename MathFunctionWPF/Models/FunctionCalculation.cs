642ef683abdefabd84aae298ba9aea9baa0004be
Станислав Хозяенок <st.hoz.kem@gmail.com>
2024-09-24 21:22:31 +0700

    Update

--- MathFunctionWPF/Models/FunctionCalculation.cs
+++ MathFunctionWPF/Models/FunctionCalculation.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using org.mariuszgromada.math.mxparser;
+﻿using org.mariuszgromada.math.mxparser;
 
 namespace MathFunctionWPF.Models
 {
